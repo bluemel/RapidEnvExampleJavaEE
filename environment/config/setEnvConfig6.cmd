3ace22b8f6243cc73d3ebc8912b5ce251f21d657
unknown <z0001wdm@mhfr015d.ww002.siemens.net>
2012-01-22 18:00:34 +0100

    First slice of update from JBoss 6 to JBoss 7

---
+++ environment/config/setEnvConfig6.cmd
@@ -0,0 +1,4 @@
+@echo off
+copy "%RAPID_ENV_HOME%\env_6.xml" "%RAPID_ENV_HOME%\env.xml"
+copy "%RAPID_ENV_HOME%\config\eclipse\runtimes_6.xml" "%RAPID_ENV_HOME%\config\eclipse\runtimes.xml" 
+copy "%RAPID_ENV_HOME%\config\eclipse\servers_6.xml" "%RAPID_ENV_HOME%\config\eclipse\servers.xml" 
---
+++ environment/config/setEnvConfig7.cmd
@@ -0,0 +1,4 @@
+@echo off
+copy "%RAPID_ENV_HOME%\env_7.xml" "%RAPID_ENV_HOME%\env.xml"
+copy "%RAPID_ENV_HOME%\config\eclipse\runtimes_7.xml" "%RAPID_ENV_HOME%\config\eclipse\runtimes.xml" 
+copy "%RAPID_ENV_HOME%\config\eclipse\servers_7.xml" "%RAPID_ENV_HOME%\config\eclipse\servers.xml" 
